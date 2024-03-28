757e7d875aec6d15ffb123bb92638999097359ae
georgesroberto <georgesroberto21@gmail.com>
2024-03-28 14:52:57 +0300

    #02 Complete Basic Functionality

---
+++ math.js
@@ -0,0 +1,35 @@
+// Mathematical Computation
+
+// ADDITION .
+
+const add = (num1, num2) => {
+  // Add num1 to num 2 and store it in a variable sum
+  let sum = num1 + num2;
+
+  // Return the sum
+  return sum;
+};
+
+// SUBTRACTION .
+
+const subtract = (num1, num2) => {
+  // Subtract num1 from num 2 and store it in a variable output
+  let output = num2 - num2;
+
+  // Return the output
+  return output;
+};
+
+// REMAINDER .
+const remainder = (num1, num2) => {
+  // Find the remainder of num1 divided by num2
+  let rem = num1 % num2;
+
+  // Return the sum
+  return rem;
+};
+
+// MULTIPLICATION .
+const multiply = (num1, num2) => {
+  /* TODO: Implement multiplication */
+};
